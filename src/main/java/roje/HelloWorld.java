f581be8ef8618d1a02eff88e5115b2e2c8583f9d
OlivierLF <olivier.lefloch96@gmail.com>
2017-10-18 14:47:56 +0200

    Premier commit

---
+++ src/main/java/roje/HelloWorld.java
@@ -0,0 +1,10 @@
+package roje;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		LocalTime currentTime=new LocalTime();
+		System.out.println("Time=" + currentTime);
+	}
+}
