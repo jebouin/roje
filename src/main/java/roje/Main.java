99dfc76f6e7a2000207d4b1902ae27464981fdaa
Unknown <olivier.lefloch96@gmail.com>
2017-11-05 17:38:14 +0100

    add comic search by name prefix from the Marvel API

--- src/main/java/roje/Main.java
+++ src/main/java/roje/Main.java
@@ -1,7 +1,6 @@
 package roje;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -38,6 +37,18 @@ public class Main extends Application {
 		}
 	}
 
+	public void showComicSearchView() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/ComicSearchView.fxml"));
+			AnchorPane comicSearchView = (AnchorPane) loader.load();
+			rootLayout.setCenter(comicSearchView);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public Stage getPrimaryStage() {
 		return primaryStage;
 	}
@@ -49,9 +60,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Rojé Comics Manager");
+		this.primaryStage.setTitle("Roje Comics Manager");
 		initRootLayout();
-		showCharacterSearchView();
+		showComicSearchView();
 	}
 
 	public static void main(String[] args) throws Exception {
--- src/main/java/roje/api/MarvelAPI.java
+++ src/main/java/roje/api/MarvelAPI.java
@@ -50,4 +50,35 @@ public class MarvelAPI {
 		return names;
 	}
 
+	public static List<String> searchComicsByNamePrefix(String titleSearch) throws Exception {
+		String timestamp = new Long(System.currentTimeMillis()).toString();
+		String hash = DigestUtils.md5Hex((timestamp + privateKey + publicKey).getBytes());
+		String path = "https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=" + titleSearch + "&apikey="
+				+ publicKey + "&ts=" + timestamp + "&hash=" + hash;
+		StringBuilder result = new StringBuilder();
+		URL url = new URL(path);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		while ((line = rd.readLine()) != null) {
+			result.append(line);
+		}
+		rd.close();
+
+		// TODO return more information (short description, image)
+		List<String> titles = new ArrayList<String>();
+		JsonObject jsonObject = new JsonParser().parse(result.toString()).getAsJsonObject();
+		JsonArray data = jsonObject.get("data").getAsJsonObject().get("results").getAsJsonArray();
+		for (Iterator<JsonElement> it = data.iterator(); it.hasNext();) {
+			JsonObject el = it.next().getAsJsonObject();
+			String title = el.get("title").toString();
+			// get rid of the quotes
+			title = title.substring(1, title.length() - 1);
+			titles.add(title);
+		}
+		System.out.println(titles.size());
+		return titles;
+	}
+
 }
